6ce84f0b8f0a6264e074e9472f75a63e7ab76c11
AdrienHeisch <adrienheisch@orange.fr>
2018-05-24 02:50:52 +0200

    add image display, change to dev mode

--- src/client/main.js
+++ src/client/main.js
@@ -1,8 +1,32 @@
 import Point from "../shared/point.js";
+import shipImage from "../assets/images/ship.png";
 
 export default function init() {
     console.log('init!');
     console.log($(window));
+
+    /*let img = new Image();
+    img.src = shipImage;
+    img.onload = () => $("#root").append(img);
+    img.onerror = () => console.log("Couldn't load ship.png...") */
+
+    /*(async function() {
+        $("#root").append(await (() => {
+            let img = new Image();
+            img.src = shipImage;
+            return img;
+        }));
+    })();*/
+
+    new Promise((resolve, reject) => {
+        let img = new Image();
+        img.src = shipImage;
+        img.onload = () => resolve(img);
+        img.onerror = () => reject(shipImage);
+    }).then(
+        img => $("#root").append(img),
+        src => console.log("Could not load image", src)
+    );
 
     console.log(new Point(5, 2));
 }
--- webpack.config.js
+++ webpack.config.js
@@ -1,7 +1,7 @@
 const webpack = require("webpack");
 const path = require("path");
 
-const mode = "production";
+const mode = "development";
 
 module.exports = [
     {
@@ -19,11 +19,13 @@ module.exports = [
             net: "empty"
         },
         module: {
-            rules: [{
-                test: /\.js$/,
-                exclude: /node_modules/,
-                loader: "babel-loader"
-            }]
+            rules: [
+                {
+                    test: /\.js$/,
+                    exclude: /node_modules/,
+                    loader: "babel-loader"
+                }
+            ]
         }
     },
     {
@@ -42,6 +44,12 @@ module.exports = [
                     use: [
                         'style-loader',
                         'css-loader'
+                    ]
+                },
+                {
+                    test: /\.(png|svg|jpg|gif)$/,
+                    use: [
+                        'file-loader'
                     ]
                 }
             ]
