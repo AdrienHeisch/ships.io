972486f7b4c1802292e4589786d4e9dbebd22fac
AdrienHeisch <adrienheisch@orange.fr>
2018-05-29 12:25:31 +0200

    fix heroku git lfs problem

--- src/server/app.js
+++ src/server/app.js
@@ -10,10 +10,16 @@ const port = process.env.PORT || 8080;
 export default ({
     start() {
         app.use(expresslfs('https://github.com/AdrienHeisch/ships.io.git/info/lfs/objects/batch', {
-            publicpath: "./public/",
             oids
         }));
+        app.use(function (req, res, next) {
+            console.log(req.url);
+            if (req.url === '/textures.png') {
+                return;
+            } else next();
+        });
         app.use(express.static('./dist/public/'));
+
         server.listen(port);
         console.log('Listening on port', port, '!');
     },
